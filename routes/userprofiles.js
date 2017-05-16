0cb921bf810dbecd4ec69260370abd81fd176622
Jens De Weerdt <jdwvideos@gmail.com>
2017-05-16 19:36:45 +0200

    user info shows for every user

--- routes/userprofiles.js
+++ routes/userprofiles.js
@@ -2,17 +2,20 @@ var express = require('express');
 var router = express.Router();
 var userController = require('../controllers/user');
 var User = require('../models/user');
-
+var Group = require('../models/group');
 
 router.get('/:i', userController.isLoggedIn,function(req, res, next) {
+    Group.find(function(err, groups){    
         User.find(function(err, users){
         res.render('useradmin', {
         user : req.user,
         users: users,
-        g: req.params.i
+        groups: groups,
+        selecteduser: req.params.i
         })
     
     });
+    })
         
 });
 
