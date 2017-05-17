60ac8c9e8001b4cb7ab51e5cd42f7c62133328db
Fabio Van Dooren <fabio.vandooren@hotmail.com>
2017-05-17 14:00:35 +0200

    search list van users omgezet naar ul li list met onzichtbare bullet

--- app.js
+++ app.js
@@ -5,8 +5,25 @@ var passport   = require('passport');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var session = require('express-session');
+var socket_io = require('socket.io');
 
 var app = express();
+//Socket.io
+/*var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+server.listen(80);
+
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
+
+io.on('connection', function (socket) {
+  socket.emit('news', { hello: 'world' });
+  socket.on('my other event', function (data) {
+    console.log(data);
+  });
+});*/
 
 mongoose.connect('mongodb://localhost/demoordie');
 // pass passport for configuration
@@ -44,6 +61,13 @@ app.get('/auth/facebook/callback',
 app.listen(3000, function () {
     console.log('Example app listening on port 3000!');
 })
+
+
+//socket.io events
+/*io.on("connection", function(socket){
+    console.log("a user is connected");
+});*/
+
 
 
 
